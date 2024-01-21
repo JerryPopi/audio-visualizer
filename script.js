9d166f241e658ff88986a341c3ff1fefb19ab91a
JerryPopi <45138831+JerryPopi@users.noreply.github.com>
2024-01-21 21:01:32 +0200

    circle

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const audioPath = "gydra_ikra.opus";
 // const audioPath = "./Matroda_-_No Sleep_(6AM).opus";
-const audio = new Audio(audioPath);
-const audioCtx = new AudioContext();
+// const audio = new Audio(audioPath);
+const audio = document.querySelector("#audioctr");
 
 const container = document.querySelector('#container');
 const canvas = document.querySelector('#cnv');
@@ -9,17 +9,19 @@ canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 const ctx = canvas.getContext('2d');
 
+
 let audioSrc;
 let analyser;
+let audioCtx;
+const fftSize = 512;
+audio.addEventListener('play', () => {
+    audioCtx ??= new AudioContext();
 
-container.addEventListener('click', () => {
-    audio.play();
-    // setTimeout(() => audio.pause(), 3000);
-    audioSrc = audioCtx.createMediaElementSource(audio);
-    analyser = audioCtx.createAnalyser();
+    audioSrc ??= audioCtx.createMediaElementSource(audio);
+    analyser ??= audioCtx.createAnalyser();
     audioSrc.connect(analyser);
     analyser.connect(audioCtx.destination);
-    analyser.fftSize = 256;
+    analyser.fftSize = fftSize;
 
     const bufLen = analyser.frequencyBinCount;
     const data = new Uint8Array(bufLen);
@@ -28,20 +30,46 @@ container.addEventListener('click', () => {
     let barHeight;
 
     function animate(){
-        let x = 0;
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-        analyser.getByteFrequencyData(data);
+        if(!audio.paused) {
+            ctx.clearRect(0, 0, canvas.width, canvas.height);
+            analyser.getByteFrequencyData(data);
 
-        for(let i = 0; i < bufLen; ++i) {
-            // barHeight = data[i] * 1.5;
-            barHeight = Math.pow(data[i], 1.5) / 10
-            ctx.fillStyle = `hsl(${Math.min(i * 3, 359)}, 100%, 50%)`;
-            console.log(ctx.fillStyle)
-            ctx.fillRect(x, canvas.height - barHeight, barWidth, barHeight);
-            x += barWidth;
+            // let x = 0;
+            // for(let i = 0; i < bufLen; ++i) {
+            //     // barHeight = data[i] * 1.5;
+            //     barHeight = Math.pow(data[i], 1.5) / 10
+            //     ctx.fillStyle = `hsl(${Math.min(i * 3, 359)}, 100%, 50%)`;
+            //     // console.log(ctx.fillStyle)
+            //     ctx.fillRect(x, canvas.height - barHeight, barWidth, barHeight);
+            //     x += barWidth;
+            // }
+
+            const ratio = 360 / bufLen;
+            const cx = canvas.width / 2;
+            const cy = canvas.height / 2;
+            const r  = 60;
+
+            const mapval = (x, in_min = 0, in_max = 360, out_min = 0, out_max = bufLen) =>
+            {
+                return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+            }
+
+            for(let i = 0; i < 360; i += ratio) {
+                ctx.save()
+                const rad = Math.PI / 180 * i;
+
+                barHeight = Math.pow(data[mapval(i)], 1);
+
+                ctx.fillStyle = `hsl(${Math.min(i, 359)}, 100%, 50%)`;
+                ctx.translate(cx + Math.cos(rad) * r, cy + Math.sin(rad) * r);
+                ctx.rotate(rad - Math.PI / 2);
+                ctx.fillRect(0, 0, barWidth, barHeight);
+                ctx.restore()
+            }
         }
 
-        requestAnimationFrame(animate);
+        setTimeout(() => requestAnimationFrame(animate), 5);
+        // requestAnimationFrame(animate);
     }
     animate();
 });
